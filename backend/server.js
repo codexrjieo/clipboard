2a87eb40123e1600a11cdf453aa226b02353f63d
Rohit Raj <codexrohit0@gmail.com>
2025-07-02 10:42:34 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -26,14 +26,6 @@ app.get("/", (req, res) => {
 
 const path = require("path");
 
-// Serve frontend static files in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"));
-  });
-}
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
